262641b6db9968655404b8eeba89f107fcee4f4f
faisaal619 <faisal.alasadi92@gmail.com>
2022-06-24 17:57:29 -0700

    Create-Website-Layout

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,14 @@
 import Head from 'next/head'
  import { Typography } from '@mui/material';
+import Layout from '../components/Layout';
 
 
  export default function Home() {
    return (
 
-     <div>
-       <Head>
-
-         <title>Sanity Lions Sportwear</title>
-         <meta
-           name="description"
-           content="The ecommerce website by next and sanity"
-         />
-         <link rel="icon" href="/favicon.ico" />
-       </Head>
-
-      
-       <Typography component="h1" variant="h1">
-         Sanity Lions Sportwear
-       </Typography>
-     </div>
+    <Layout>
+      List Products
+    </Layout>
    
    );
  }
