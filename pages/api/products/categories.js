1778b3a84ce246a285fae6b1a0a29cc7ed0f66e2
faisaal619 <faisal.alasadi92@gmail.com>
2022-06-28 20:16:51 -0700

    Spellings

--- pages/api/products/categories.js
+++ pages/api/products/categories.js
@@ -3,7 +3,7 @@ import nc from 'next-connect';
 const handler = nc();
 
 handler.get(async (req, res) => {
-  const categories = ['Jerseys', 'Hooddies', 'Track Suit'];
+  const categories = ['Jerseys', 'Hoodies', 'Track Suit', 'Accessories'];
   res.send(categories);
 });
 
